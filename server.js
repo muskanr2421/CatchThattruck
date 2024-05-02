9ea984863443a2a250bd266ba8d7819bbf6d0506
muskan.r2421 <muskan.r@cisinlabs.com>
2024-05-02 18:55:48 +0530

    Notification Update

--- server.js
+++ server.js
@@ -352,10 +352,10 @@ async function updateVendorLocation(lat, long, id, socket) {
 
       const distance = calculateDistanceMiles(userLat, userLong, lat, long)
       console.log("Distance", distance)
-      console.log("Notifi", notifi[id])
-      if (!notifi[id]) {
+      console.log("Notifi", notifi[userId])
+      if (!notifi[userId]) {
         if (distance <= truckDetail.first_alert) {
-          notifi[id] = true;
+          notifi[userId] = true;
           if (distance <= truckDetail.second_alert) {
             if(data.fcm_token){
               middleware.CustomNotification("Truck Alert", `${truckDetail.truck_name} is pretty close to you`, data.fcm_token)
